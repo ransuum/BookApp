d81248670c444ada6da3c4b6a1b7d27e2ec39b32
ransuum <dmitrenkovalera9@gmail.com>
2023-10-07 12:54:35 +0300

    Initial commit

---
+++ src/main/java/org/bookmap/Main.java
@@ -0,0 +1,59 @@
+package org.bookmap;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        OrderBook orderBook = new OrderBook();
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] array = line.split(",");
+                String action = array[0];
+                if ("u".equals(action)) {
+                    if (array.length >= 4) {
+                        int price = Integer.parseInt(array[1]);
+                        int size = Integer.parseInt(array[2]);
+                        String orderType = array[3];
+                        orderBook.LimitOrder(orderType, price, size);
+                    } else {
+                        writer.write("Error");
+                        writer.newLine();
+                    }
+                } else if ("q".equals(action)) {
+                    String queryType = array[1];
+                    if ("best_bid".equals(queryType) || "best_ask".equals(queryType)) {
+                        String result = orderBook.getBestPriceAndSize(queryType);
+                        writer.write(result);
+                        writer.newLine();
+                    } else if ("size".equals(queryType) && array.length >= 4) {
+                        int price = Integer.parseInt(array[2]);
+                        int size = orderBook.getSizeAtPrice(array[3], price);
+                        StringBuilder result = new StringBuilder();
+                        result.append(price).append(",").append(size);
+                        writer.write(result.toString());
+                        writer.newLine();
+                    }
+                } else if ("o".equals(action)) {
+                    if (array.length >= 3) {
+                        String orderType = array[1];
+                        int size = Integer.parseInt(array[2]);
+
+                        if ("buy".equals(orderType)) {
+                            orderBook.MarketOrder(orderType, size);
+                        } else if ("sell".equals(orderType)) {
+                            orderBook.MarketOrder(orderType, size);
+                        }
+                        writer.write(Integer.toString(size));
+                        writer.newLine();
+                    }
+                }
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("execution time: " + (endTime - startTime) + " ms");
+    }
+}
---
+++ src/main/java/org/bookmap/OrderBook.java
@@ -0,0 +1,69 @@
+package org.bookmap;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class OrderBook {
+    private final TreeMap<Integer, Integer> bidOrders;
+    private final TreeMap<Integer, Integer> askOrders;
+
+    public OrderBook(){
+        bidOrders = new TreeMap<>(Collections.reverseOrder());
+        askOrders = new TreeMap<>();
+    }
+    public void LimitOrder(String type, int size, int price) {
+        if ("bid".equals(type)) {
+            if (size > 0) {
+                bidOrders.put(price, size);
+            } else {
+                bidOrders.remove(price);
+            }
+        } else if ("ask".equals(type)) {
+            if (size > 0) {
+                askOrders.put(price, size);
+            } else {
+                askOrders.remove(price);
+            }
+        }
+    }
+
+    public void MarketOrder(String type, int size) {
+        TreeMap<Integer, Integer> orders;
+
+        if ("buy".equals(type)) {
+            orders = askOrders;
+        } else if ("sell".equals(type)) {
+            orders = bidOrders;
+        } else {
+            return;
+        }
+        int remainingSize = size;
+        for (Map.Entry<Integer, Integer> entry : orders.entrySet()) {
+            int price = entry.getKey();
+            int orderSize = entry.getValue();
+            if (remainingSize >= orderSize) {
+                remainingSize -= orderSize;
+                orders.remove(price);
+            } else {
+                orders.put(price, orderSize - remainingSize);
+                break;
+            }
+        }
+    }
+    public String getBestPriceAndSize(String type) {
+        Map<Integer, Integer> orders = ("best_bid".equals(type)) ? bidOrders : askOrders;
+        if (orders.isEmpty()) {
+            return "0,0";
+        }
+        int bestPrice = type.equals("best_bid") ? orders.keySet().stream().max(Integer::compareTo).get() :
+                orders.keySet().stream().min(Integer::compareTo).get();
+        int bestSize = orders.get(bestPrice);
+        return bestPrice + "," + bestSize;
+    }
+
+    public int getSizeAtPrice(String type, int price) {
+        Map<Integer, Integer> orders = ("bid".equals(type)) ? bidOrders : askOrders;
+        return orders.getOrDefault(price, 0);
+    }
+}
